e316282acdf46c7ffa996b4f4a162d5bbad19326
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-03-20 18:40:44 +0000

    social victor

--- src/js/app.js
+++ src/js/app.js
@@ -29,21 +29,30 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // Se usa un operador ternario para asignar la clase de posición según el valor recibido.
+  // Se comprueba si socialMediaPosition es "position-left" o "position-right".
+  let socialMediaClass =
+    variables.socialMediaPosition === "position-left"
+      ? "position-left"
+      : variables.socialMediaPosition === "position-right"
+      ? "position-right"
+      : "position-right"; // valor por defecto
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+                ${cover}
+              <img src="${variables.avatarURL}" class="photo" />
+              <h1>Lucy Boilett</h1>
+              <h2>Web Developer</h2>
+              <h3>Miami, USA</h3>
+              <ul class="${socialMediaClass}">
+                <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
+                <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+              </ul>
+            </div>
+        `;
 }
 
 /**
